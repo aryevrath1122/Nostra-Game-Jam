3a1696c4333a28b280d9a919c161d78a35b78124
aryevrath1122 <garyevrath@gmail.com>
2024-11-29 18:28:11 +0530

    final update
    ...

--- Assets/Scenes/UI/MainUiController.cs
+++ Assets/Scenes/UI/MainUiController.cs
@@ -1,19 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+using TMPro;
 
 public class MainUiController : MonoBehaviour
 {
+    public GameObject PlayButton;
+    public GameObject QuitButton;
+    public GameObject AboutButton;
+    public GameObject MainMenu;
+    public GameObject AboutPanel;
     
+    public string sceneToLoad = "LVL 1";
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
-        
+        AboutPanel.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+   
+    public void QuitBut()
     {
-        
+        Application.Quit();
     }
+    public void PlayBut()
+    {
+        Debug.Log("Pressed");
+        SceneManager.LoadScene(sceneToLoad);
+    }
+    public void AboutBut()
+    {
+       AboutPanel?.SetActive(true);
+       MainMenu?.SetActive(false);
+        PlayButton?.SetActive(false);
+        QuitButton?.SetActive(false);
+        AboutButton.SetActive(false);
+    }
+    
 }
--- Assets/Scripts/Game Manager.cs
+++ Assets/Scripts/Game Manager.cs
@@ -2,25 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     PlayerManager pm;
+    public GameObject PauseMenu;
     public TextMeshProUGUI BoltCountText; 
     // Start is called before the first frame update
     void Start()
     {
         pm = FindObjectOfType<PlayerManager>();
+        PauseMenu.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         UpdateScoreDisplay();
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Time.timeScale = 0;
+            PauseMenu?.SetActive(true);
+        }
     }
     private void UpdateScoreDisplay()
     {
         BoltCountText.text = "Bolts: "+pm.CurrentCoinsCollected;
+    }
+    public void Resume()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+    public void Restart()
+    {
+        string currentScene = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(currentScene);
+    }
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
