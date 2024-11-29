63971c4fe425de6037965fc69daa229d336437fa
DarkSoloNecromancer <leninchandrakiran@gmail.com>
2024-11-29 17:37:04 +0530

    Lv 1 completed
    ...

--- Assets/Scripts/Game Manager.cs
+++ Assets/Scripts/Game Manager.cs
@@ -2,21 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public float TutorialTextTimer = 10f;
-    public TextMeshPro TutorialText;
-    public TextMeshPro BoltCount; 
+    PlayerManager pm;
+    public TextMeshProUGUI BoltCountText; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        pm = FindObjectOfType<PlayerManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateScoreDisplay();
+    }
+    private void UpdateScoreDisplay()
+    {
+        BoltCountText.text = "Bolts: "+pm.CurrentCoinsCollected;
     }
 }
